019095563557ab842f24c4b9b95c2d0509165aea
liming <827182486@qq.com>
2020-07-29 23:36:27 +0800

    添加提示信息

--- login/views.py
+++ login/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from . import models
 
+
 # Create your views here.
 def index(request):
     pass
@@ -12,13 +13,21 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         print(username, password)
+        message = '请检查填写的内容！'
         if username.strip() and password:
             try:
                 user = models.User.objects.get(name=username)
             except:
-                return render(request, 'login/login.html')
+                message = '用户不存在！'
+                return render(request, 'login/login.html', {'message': message})
             if user.password == password:
+                print(username, password)
                 return redirect('/index/')
+            else:
+                message = '密码不正确！'
+                return render(request, 'login/login.html', {'message': message})
+        else:
+            return render(request, 'login/login.html', {'message': message})
     return render(request, 'login/login.html')
 
 
